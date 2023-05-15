904ca9d3ee48c9789c352eda6726ac2611fc99e7
MatheusEinloft <matheuseinloft97@gmail.com>
2023-05-14 23:47:49 -0300

    criando formulario para cadastrar o usuario

--- app/Controllers/Usuarios.php
+++ app/Controllers/Usuarios.php
@@ -4,6 +4,23 @@ class Usuarios extends Controller {
 
     public function cadastrar(){
 
-        $this->view('usuarios/cadastrar');
+        $formulario = filter_input_array(INPUT_POST,FILTER_SANITIZE_STRING);
+         if(isset($formulario)){
+            $dados = [ 
+                'nome' => trim($formulario['nome']),
+                'email' => trim($formulario['email']),
+                'senha' => trim($formulario['senha']),
+                'confirma_senha' => trim($formulario['confirma_senha']),
+            ];   
+        } else {
+            $dados = [
+                'nome' => '',
+                'email' => '',
+                'senha' => '',
+                'confirma_senha' => '',
+            ];
+        }
+        
+        $this->view('usuarios/cadastrar', $dados);
     }
 }
--- app/Views/usuarios/cadastrar.php
+++ app/Views/usuarios/cadastrar.php
@@ -1,27 +1,30 @@
 <div class="col-xl-4 col-md-6 mx-auto p-5">
     <div class="card">
+        <div class="card-header">
+            Cadastre-se
+        </div>
         <div class="card-body">
-            <h2>Cadastrar-se</h2>
-            <small>Preencha o formulário abaixo para fazer seu cadastro.</small>
-            <form name="cadastrar" method="POST" action="">
+            <p class="card-text"><small>Preencha o formulário abaixo para fazer seu cadastro.</small></p>
+
+            <form name="cadastrar" method="POST" action="<?= URL?>/usuarios/cadastrar" class="mt-4">
                 <div class="form-group"> 
                     <label for="nome">Nome: <sup class="text-danger">*</sup></label>
-                    <input type="text" name="nome" id="nome" class="form-control" required>
+                    <input type="text" name="nome" id="nome" value="<?=$dados['nome']?>" class="form-control" required>
                 </div>
                 
                 <div class="form-group"> 
                     <label for="email">E-mail: <sup class="text-danger">*</sup></label>
-                    <input type="text" name="email" id="email" class="form-control" required>
+                    <input type="text" name="email" id="email" value="<?=$dados['email']?>" class="form-control" required>
                 </div>
 
                 <div class="form-group"> 
                     <label for="email">Senha: <sup class="text-danger">*</sup></label>
-                    <input type="password" name="senha" id="senha" class="form-control" required>
+                    <input type="password" name="senha" id="senha" value="<?=$dados['senha']?>" class="form-control" required>
                 </div>
 
                 <div class="form-group"> 
-                    <label for="confirmar_senha">Confirme a senha: <sup class="text-danger">*</sup></label>
-                    <input type="password" name="confirmar_senha" id="confirmar_senha" class="form-control" required>
+                    <label for="confirma_senha">Confirme a senha: <sup class="text-danger">*</sup></label>
+                    <input type="password" name="confirma_senha" id="confirma_senha" value="<?=$dados['confirma_senha']?>" class="form-control" required>
                 </div> 
 
                 <div class="row">
